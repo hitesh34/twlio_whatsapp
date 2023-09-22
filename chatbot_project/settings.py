46bc52f7a8fe6f194052f9db2ceb92aaebcc4d5b
hitesh34 <hiteshmansinghani@gmail.com>
2023-09-22 16:49:56 +0530

    new commit

--- chatbot_project/settings.py
+++ chatbot_project/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bc&f%y^3ht6rp_tkz-5i!7mq!gr77()0=$@+4-n-u*rf)q$4p&'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
