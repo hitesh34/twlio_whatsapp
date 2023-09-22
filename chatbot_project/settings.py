c5c10ff4cb1a83db5c6f9d480081cf20132017ff
hitesh34 <79905249+hitesh34@users.noreply.github.com>
2023-09-22 13:41:13 +0530

    Update settings.py

--- chatbot_project/settings.py
+++ chatbot_project/settings.py
@@ -120,8 +120,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
